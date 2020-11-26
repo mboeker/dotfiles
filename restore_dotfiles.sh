3aa53ee1859129e8ecd9bcfb80c0784331fab081
mboeker <mboeker@gmail.com>
2020-11-26 16:27:13 +0100

    restore 2015

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -3,10 +3,15 @@ cd
 git clone https://github.com/mboeker/dotfiles
 cd dotfiles
 git pull origin master
-cd
 
-# transfer dotfiles
-rsync -a --exclude .git --exclude restore_dotfiles.sh  ~/dotfiles/ ~/
+## transfer dotfiles
+#rsync -a --exclude .git --exclude restore_dotfiles.sh  ~/dotfiles/ ~/
+#rsync -a ~/dotfiles/restore_dotfiles.sh ~/bin/
+
+# symlink dotfiles
+cd
+for t in .atom .bash_profile .pip .profile .ssh .vim .vimrc .slate .zshrc filezilla karabiner; do
+   rm -rf $t; ln -s dotfiles/$t $t
+done
+
 rsync -a ~/dotfiles/restore_dotfiles.sh ~/bin/
-#rsync -a  ~/dotfiles/.* ~
-#rm -rf dotfiles
